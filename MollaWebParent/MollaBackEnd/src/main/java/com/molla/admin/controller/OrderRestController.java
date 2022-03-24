e6a3c59be60a0e0d0e6e105b50aa09a0d2d94f21
Mohamed Hashad <mohamed085555@gmail.com>
2022-03-24 16:10:47 +0200

    update order status

--- MollaWebParent/MollaBackEnd/src/main/java/com/molla/admin/controller/OrderRestController.java
+++ MollaWebParent/MollaBackEnd/src/main/java/com/molla/admin/controller/OrderRestController.java
@@ -2,7 +2,7 @@ package com.molla.admin.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.molla.admin.dto.OrderResponseDTO;
@@ -15,7 +15,7 @@ public class OrderRestController {
     @Autowired
     private OrderService service;
 
-    @PostMapping("/orders_shipper/update/{id}/{status}")
+    @GetMapping("/orders_shipper/update/{id}/{status}")
     public OrderResponseDTO updateOrderStatus(@PathVariable("id") Integer orderId,
                                               @PathVariable("status") String status) {
         service.updateStatus(orderId, status);
